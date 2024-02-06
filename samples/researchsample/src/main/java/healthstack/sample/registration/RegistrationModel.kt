8e0b0811b8d2d63c0e958d44729cf5dd08ae5a52
Jeongdong Hwang <jade.hwang@samsung.com>
2024-02-06 14:27:02 +0900

    :moyai: Remove sample app directory

--- samples/researchsample/src/main/java/healthstack/sample/registration/RegistrationModel.kt
+++
@@ -1,9 +0,0 @@
-package healthstack.sample.registration
-
-import healthstack.kit.task.base.StepModel
-import healthstack.kit.task.survey.question.model.QuestionModel
-
-class RegistrationModel(
-    title: String,
-    val eligibilityQuestions: List<QuestionModel<Any>>
-) : StepModel("", title, null)
